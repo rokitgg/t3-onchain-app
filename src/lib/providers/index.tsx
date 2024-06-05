b4be3c6a1cea9ca0d2dcb5af67a58dd44ec85158
rokitgg <pintosdsgn@gmail.com>
2024-06-05 02:21:37 +0200

    refactor: Update file location for wagmi configuration

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState, type ReactNode } from 'react'
 import { WagmiProvider } from 'wagmi'
 
-import { config } from "@/lib/wagmi"
+import { config } from "@/server/wagmi"
 
 export function ContextProvider(props: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
